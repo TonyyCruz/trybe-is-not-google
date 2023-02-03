630461d941537383aa8036292c6e02c448113c43
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-03 16:14:51 -0300

    feat: requisito 2 - implementa funcao "txt_importer" recebe o caminho para um arquivo ".txt" e retorna seu conteudo em uma lista separada por quebra di linha "\n"

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            file_data = file.read()
+            file_list = file_data.split("\n")
+            return file_list
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
