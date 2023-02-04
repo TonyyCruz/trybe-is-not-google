b1bb348d5f70f14ca912d4167975a87eef91049b
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-04 14:30:44 -0300

    feat: requisito 7 - implementa a funcao "exists_wors" recebe como parametro uma palavra e uma instance da classe "Queue" e retorna um dicionario informando os arquivos em que a palavra buscada foi encontrada, a palavra que foi buscada e em quais linhas a palavra foi encontrada

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -15,7 +15,7 @@ mock_priority_data_four = {
 }
 
 mock_common_data = {
-    "nome_do_arquivo": "arquivo_priority.txt",
+    "nome_do_arquivo": "arquivo_common.txt",
     "qtd_linhas": 6,
     "linhas_do_arquivo": ["a", "b", "c", "d", "e", "f"]
 }
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = list()
+
+    for idx in range(instance.__len__()):
+        queue_data = instance.search(idx)
+        words_found_dict = {
+            "palavra": word,
+            "arquivo": queue_data["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for position, string in enumerate(queue_data["linhas_do_arquivo"]):
+            if word.lower() in string.lower():
+                words_found_dict["ocorrencias"].append({"linha": position + 1})
+        if len(words_found_dict["ocorrencias"]):
+            response.append(words_found_dict)
+    return response
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    print("llllllllllllllll", word, instance)
